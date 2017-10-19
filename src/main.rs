7f239609ab6f6b6a9ab41fb557d6e535ebb559ed
Kojo Adams <kadams85@bloomberg.net>
2017-10-19 13:16:02 -0400

    Remove debug code

--- src/main.rs
+++ src/main.rs
@@ -11,8 +11,6 @@ fn main() {
     let mut file = File::open(file).unwrap();
     file.read_to_string(&mut file_contents);
 
-    println!("{}", file_contents);
-    
     let ast  = babel_parsing::parser::parse_TopLevel(&file_contents);
     println!("{:?}", ast);
 }
