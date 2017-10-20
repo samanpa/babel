16f90caf74f4e347d3f45ff8b601f9d3cdd22af1
Kojo Adams <samanpa@gmail.com>
2017-10-19 22:22:21 -0400

    Refactoring: move parser and main into one module

---
+++ TODO.md
@@ -0,0 +1,1 @@
+- move to menhir grammar
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-extern crate babel_parsing;
+extern crate babel;
 
 use std::env;
 use std::fs::File;
@@ -9,10 +9,8 @@ fn main() {
     let mut file_contents = String::new();
     let file = env::args().nth(1).unwrap();
     let mut file = File::open(file).unwrap();
-    file.read_to_string(&mut file_contents);
+    let _ = file.read_to_string(&mut file_contents);
 
-    println!("{}", file_contents);
-    
-    let ast  = babel_parsing::parser::parse_TopLevel(&file_contents);
+    let ast  = babel::parser::parse_TopLevel(&file_contents);
     println!("{:?}", ast);
 }
