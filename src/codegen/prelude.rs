b68d041efd7b27d8f9660cf75e4f2419283d6e0e
Kojo Adams <kadams85@bloomberg.net>
2017-12-14 13:46:17 -0500

    Extend prelude

--- src/codegen/prelude.rs
+++ src/codegen/prelude.rs
@@ -44,11 +44,35 @@ impl Prelude {
                 let res = LLVMBuildRet(builder, add);
                 Some(res)
             }
+            "i32_mul" => {
+                Self::prepare(LLVMGetModuleContext(module), func, builder);
+                let p0  = LLVMGetParam(func, 0);
+                let p1  = LLVMGetParam(func, 1);
+                let mul = LLVMBuildMul(builder, p0, p1, label("mul").as_ptr());
+                let res = LLVMBuildRet(builder, mul);
+                Some(res)
+            }
+            "i32_div" => {
+                Self::prepare(LLVMGetModuleContext(module), func, builder);
+                let p0  = LLVMGetParam(func, 0);
+                let p1  = LLVMGetParam(func, 1);
+                let div = LLVMBuildSDiv(builder, p0, p1, label("div").as_ptr());
+                let res = LLVMBuildRet(builder, div);
+                Some(res)
+            }
+            "i32_rem" => {
+                Self::prepare(LLVMGetModuleContext(module), func, builder);
+                let p0  = LLVMGetParam(func, 0);
+                let p1  = LLVMGetParam(func, 1);
+                let rem = LLVMBuildSRem(builder, p0, p1, label("rem").as_ptr());
+                let res = LLVMBuildRet(builder, rem);
+                Some(res)
+            }
             "i32_lt" => {
                 Self::prepare(LLVMGetModuleContext(module), func, builder);
                 let p0  = LLVMGetParam(func, 0);
                 let p1  = LLVMGetParam(func, 1);
-                let op  = LLVMIntPredicate::LLVMIntSGT;
+                let op  = LLVMIntPredicate::LLVMIntSLT;
                 let add = LLVMBuildICmp(builder, op, p0, p1, label("lt").as_ptr());
                 let res = LLVMBuildRet(builder, add);
                 Some(res)
