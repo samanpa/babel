8c7fc403116044ddc426ce0daf4bcc0e8a0c10c1
Paa Kojo Samanpa <samanpa@gmail.com>
2019-06-21 19:56:30 -0400

    2018 changes

--- src/ast.rs
+++ src/ast.rs
@@ -69,7 +69,7 @@ pub fn con(nm: &str, kind: crate::types::Kind) -> Type {
         "->" => Func,
         _ => NewType(std::rc::Rc::new(nm.to_string())),
     };
-    types::Type::Con(tycon, kind)
+    crate::types::Type::Con(tycon, kind)
 }
 
 impl Decl {
